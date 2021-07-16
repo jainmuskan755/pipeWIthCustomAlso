d2d0df1c91a95aefc321366df9cd73a8b0badbf7
Muskan Jain <muskan.jain@biz2credit.org>
2021-07-16 17:50:59 +0530

    files uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,42 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+
+
   title = 'custom';
+  searchText;
+  todaydate = new Date();  
+  jsonval = {name: 'Alex', age: '25', address:{a1: 'Paris', a2: 'France'}};  
+  months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'Jun',  
+    'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];  
+    str = 'searchStr';
+
+    names = [
+      '8aJani',
+      'Carl',
+      'M8aargareth',
+      'He8ge',
+      'Jo8e',
+      'J8austav',
+      'B8irgit',
+      '8aMary',
+      '8Kai'
+  ];
+
+  // Convert Number to Words in English and Spanish
+
+  constructor() { 
+    //var require: any;
+    // var writtenForm = require('written-number');
+    // var wf = writtenForm(100000); 
+    // console.log(wf);
+    // writtenForm.defaults.lang = 'es';
+    // var wf = writtenForm(28934355); 
+    // console.log(wf);
+  }
+
+  
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SqrtPipe } from './sqrtPipe.component';
+import { HighlightSearch } from './highlight.component';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './filtrr.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SqrtPipe,
+    HighlightSearch,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
